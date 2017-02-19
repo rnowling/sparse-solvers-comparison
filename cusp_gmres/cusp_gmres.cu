0f5535516384f616b8b62465df474ea83b8a9227
RJ Nowling <rnowling@gmail.com>
2017-02-18 21:09:04 -0600

    Read matrix files

--- cusp_gmres/cusp_gmres.cu
+++ cusp_gmres/cusp_gmres.cu
@@ -2,25 +2,78 @@
 #include <cusp/monitor.h>
 #include <cusp/krylov/gmres.h>
 #include <cusp/gallery/poisson.h>
-int main(void)
+
+#include <fstream>
+#include <stdio>
+
+void load_vector(const char* flname, cusp::array1d<float, cusp::host_memory>& v)
 {
-    // create an empty sparse matrix structure (CSR format)
-    cusp::csr_matrix<int, float, cusp::device_memory> A;
-    // initialize matrix
-    cusp::gallery::poisson5pt(A, 10, 10);
-    // allocate storage for solution (x) and right hand side (b)
-    cusp::array1d<float, cusp::device_memory> x(A.num_rows, 0);
-    cusp::array1d<float, cusp::device_memory> b(A.num_rows, 1);
-    // set stopping criteria:
-    //  iteration_limit    = 100
-    //  relative_tolerance = 1e-6
-    //  absolute_tolerance = 0
-    //  verbose            = true
-    cusp::monitor<float> monitor(b, 100, 1e-6, 0, true);
-    int restart = 50;
-    // set preconditioner (identity)
-    cusp::identity_operator<float, cusp::device_memory> M(A.num_rows, A.num_rows);
-    // solve the linear system A x = b
-    cusp::krylov::gmres(A, x, b,restart, monitor, M);
-    return 0;
+  std::ifstream input_data;
+  input_data.open(flname);
+
+  int n_elements;
+  input_data >> n_elements;
+
+  for(int i = 0; i < n_elements; i++)
+    {
+      float entry;
+      input_data >> n_elements;
+      n_elements.push_back(entry);
+    }
+
+  input_data.close();
+}
+
+void write_vector(const char* flname, cusp::array1d<float, cusp::host_memory>& v)
+{
+  std::ofstream output_data;
+  output_data.open(flname, std::ofstream::out | std::ofstream::trunc);
+
+  output_data << v.size() << std::endl;
+  for(int i = 0; i < v.size(); i++)
+    {
+      output_data << v[i] << std::endl;
+    }
+  
+  output_data.close();
+  
+}
+
+
+int main(int argc, char** argv)
+{
+  if(argc != 4)
+    {
+      std::cout << "Usage: " << argv[0] << " <matrix_flname> <input_vector_flname> <output_vector_flname>" << std::endl;
+    }
+  
+  // create an empty sparse matrix structure (CSR format)
+  cusp::csr_matrix<int, float, cusp::device_memory> A;
+  // read matrix
+  cusp::io::read_matrix_market_file(A, argv[1]);
+
+  // create empty array
+  cusp::array1d<float, cusp::host_memory> b_host(A.num_rows, 0);
+  // read vector
+  load_vector(argv[2], &b_host);
+
+  cusp::array1d<float, cusp::device_memory> b(b_host);  
+  cusp::array1d<float, cusp::device_memory> x(A.num_rows, 0);
+
+  // set stopping criteria:
+  //  iteration_limit    = 5000
+  //  relative_tolerance = 1e-6
+  //  absolute_tolerance = 0
+  //  verbose            = true
+  cusp::monitor<float> monitor(b, 5000, 1e-6, 0, true);
+  int restart = 50;
+  // set preconditioner (identity)
+  cusp::identity_operator<float, cusp::device_memory> M(A.num_rows, A.num_rows);
+  // solve the linear system A x = b
+  cusp::krylov::gmres(A, x, b,restart, monitor, M);
+
+  cusp::array1d<float, cusp::host_memory> x_host(x);
+  write_vector(argv[3], &x_host);
+  
+  return 0;
 }
